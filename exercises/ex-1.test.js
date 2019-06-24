3249f4ae26abd30c19df2b1e6defb6f05e05a4ac
Tal Bronfer <bronfer@gmail.com>
2019-06-24 21:10:34 +0300

    added tests [ci skip]

---
+++ exercises/ex-1.test.js
@@ -0,0 +1,16 @@
+import { MyApp } from '../../App';
+
+describe("exercise1", () => {     
+      it('Your add method should return the sum of the two numbers provided as arguments to the function', () => {
+          let myApp = new MyApp();
+          expect(myApp.add, "MyApp should have an add method defined").toBeDefined();
+          const result = myApp.add(1, 3);
+          expect(result, `Add method should return the sum of two numbers. We tried calling it with 1+3 and got ${result}.`).toEqual(4);
+      });     
+})
+
+
+
+
+
+
---
+++ exercises/ex-2.test.js
@@ -0,0 +1,21 @@
+import { MyApp } from '../../App';
+
+describe("exercise2", () => {   
+      it('You should define a subtract method on the MyApp class', () => {
+        let myApp = new MyApp();
+        expect(myApp.subtract, "MyApp should have a subtract method defined").toBeDefined();
+      })
+
+      it('Your subtract method should return the subtraction of the two numbers provided as arguments to the function', () => {
+          let myApp = new MyApp();
+          expect(myApp.subtract, "MyApp should have a subtract method defined").toBeDefined();
+          const result = myApp.subtract(10, 1);
+          expect(result, `Subtract method should return the subtraction of two numbers. We tried calling it with 10-1 and got ${result}.`).toEqual(9);
+      });     
+})
+
+
+
+
+
+

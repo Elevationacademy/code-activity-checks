7b61d35abc4d22ee52c4dd96c6b350e927fc11ef
Corinne <corinnegottlieb@gmail.com>
2019-07-01 15:35:30 +0300

    sc3 fix

--- spot-check-3/ex-1.test.js
+++ spot-check-3/ex-1.test.js
@@ -8,18 +8,18 @@ beforeAll( async done => {
 
 describe( 'spot-check-3', () => {
     it( 'You should create a DELETE route in your server called /wonder that accepts a name parameter, and removes that wonder from the wonders array', async done => {
-        await Client.post( 'wonder', { name: 'Colosseum', location: 'Rome, Italy' }, true )
+        await Client.post( 'wonder', { name: 'Elevation', location: 'Tel Aviv, Israel' }, true )
         await Client.post( 'wonder', { name: 'Gordon Beach', location: 'Tel Aviv, Israel' }, true )
 
-        await Client.delete( 'wonder/Colosseum' )
+        await Client.delete( 'wonder/Elevation' )
 
         const wonders = await Client.get( 'wonders' )
 
         if ( wonders === false ) {
-            fail( 'You should create a DELETE route called /wonder that removes wonder from the wonders array' )
+            fail( 'You should create a DELETE route called /wonder that removes a wonder from the wonders array' )
             done()
         } else {
-            const found = wonders.find( wonder => wonder.name === 'Colosseum' )
+            const found = wonders.find( wonder => wonder.name === 'Elevation' )
             expect( found, 'You should remove the wonder from the wonders array' ).toBeUndefined()
 
             done()

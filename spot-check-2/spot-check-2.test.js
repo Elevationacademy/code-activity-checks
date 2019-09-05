deda9cc34744a5757eaa9134758c54c24f6d01c1
corinnegottlieb <45172005+corinnegottlieb@users.noreply.github.com>
2019-09-05 15:10:37 +0300

    Update spot-check-2.test.js

--- spot-check-2/spot-check-2.test.js
+++ spot-check-2/spot-check-2.test.js
@@ -16,21 +16,21 @@ describe("spotcheck2", () => {
     const wrapper = mount(<App />);
     let spotcheck2Div = wrapper.find('#spotcheck-2')
     let companies = spotcheck2Div.find('h4')
-    expect(companies, 'There should be three h4 elements rendered on the page').toHaveLength(3)
+    expect(companies, `There should be 3 h4 elements rendered on the page, instead found ${companies.length}`).toHaveLength(3)
     
     const expected1 = 'Tesla'
     const actual1 = companies.first().text()
-    expect(actual1.toLowerCase(), `The first h4 should have the text '${expected1}'; instead found '${actual1}'`).toContain(expected1.toLowerCase())
+    expect(actual1.trim().toLowerCase(), `The first h4 should have the text '${expected1}'; instead found '${actual1}'`).toContain(expected1.toLowerCase())
     
     const expected2 = 'Microsoft'
     const actual2 = companies.at(1).text()
-    expect(actual2.toLowerCase(), `The second h4 should have the text '${expected2}'; instead found '${actual2}`).toContain(expected2.toLowerCase())
+    expect(actual2.trim().toLowerCase(), `The second h4 should have the text '${expected2}'; instead found '${actual2}`).toContain(expected2.toLowerCase())
     
     const expected3 = 'Google'
     const actual3 = companies.at(2).text()
-    expect(actual3.toLowerCase(), `The third h4 should have the text '${expected3}'; instead found '${actual3}`).toContain(expected3.toLowerCase())
+    expect(actual3.trim().toLowerCase(), `The third h4 should have the text '${expected3}'; instead found '${actual3}`).toContain(expected3.toLowerCase())
   });
-  it("The App component's render function should invoke the generateCompanyTags function", () => {
+  it("The generateCompanyTags function should be invoked in App's render function", () => {
     const wrapper = mount(<App />);
     expect(typeof wrapper.instance().generateCompanyTags, 'You must define the generateCompanyTags method in your App component').toBe('function')
     //cannot find a generic, loose way to check if arrow func was called.

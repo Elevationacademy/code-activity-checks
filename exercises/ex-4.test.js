ea240bd32da70c956395df06940f5fa66e01631f
Corinne <corinnegottlieb@gmail.com>
2019-09-09 18:36:50 +0300

    ex4 edit [ci skip]

--- exercises/ex-4.test.js
+++ exercises/ex-4.test.js
@@ -1,29 +1,16 @@
 const allFuncs = require("../../src/App")
 
 describe("exercise4", () => {
-  it('String should be reversed', () => {
-    let input = "dog"
-    let expectedOutput = "god"
-    expect(allFuncs.reverse(input)).toBe(expectedOutput)
-
-  });
-  it('String should be reversed', () => {
-    let input = "shoe"
-    let expectedOutput = "eohs"
-    expect(allFuncs.reverse(input)).toBe(expectedOutput)
-
-  });
-  it('String should be reversed including space', () => {
-    let input = "race car"
-    let expectedOutput = "rac ecar"
-    expect(allFuncs.reverse(input)).toBe(expectedOutput)
-
-  });
-  it('Emtpy string paramater should return empty string', () => {
-    let input = ""
-    let expectedOutput = ""
-    expect(allFuncs.reverse(input)).toBe(expectedOutput)
-
+  it('The function should return the string reversed', () => {
+    let input1 = "shoe"
+    let expectedOutput1 = "eohs"
+    expect(allFuncs.reverse(input1), `Output string should be the input string reversed. If ${input1} is entered, ${expectedOutput1} should be returned`).toBe(expectedOutput1)
+    let input2 = "race car"
+    let expectedOutput2 = "rac ecar"
+    expect(allFuncs.reverse(input2), `String should be reversed including the space. If ${input2} is entered, ${expectedOutput2} should be returned`).toBe(expectedOutput2)
+    let input3 = ""
+    let expectedOutput3 = ""
+    expect(allFuncs.reverse(input3), `An emtpy string paramater should return empty string. If ${input3} is entered, ${expectedOutput3} should be returned`).toBe(expectedOutput3)
   });
 })
 

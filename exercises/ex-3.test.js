62161a9c8b53a9245ea7ceb37542bbfdb2cf4356
Corinne <corinnegottlieb@gmail.com>
2019-09-10 14:32:48 +0300

    edited ex 3

--- exercises/ex-3.test.js
+++ exercises/ex-3.test.js
@@ -5,20 +5,8 @@ const storeLog = inputs => (outputData += inputs + '~')
 console["log"] = jest.fn(storeLog)
 
 describe("exercise3", () => {
-    test("Should console.log stars on separate lines in ascending order until first paramater number of stars is reached and then descending order, and then repeat as many times as second parameter number ", function () {
-    
-    outputData = ""
-    const num = 5
-    const count = 1
-    const expectedResult = "*~**~***~****~*****~****~***~**~*~~"
-    const otherResult = "*\n**\n***\n****\n*****\n****\n***\n**\n*\n~"
-    let result = allFuncs.printStarSeries(num, count)
-    allFuncs.printStarSeries(num, count)
-    expect(expectedResult == outputData || otherResult == outputData, `the output was ${result}`).toBeTruthy()
 
-})
-
-    test("Should console.log stars on separate lines in ascending order until first paramater number of stars is reached and then descending order, and then repeat as many times as second parameter number ", function () {
+    test("Should console.log stars on separate lines in ascending order until first paramater number of stars is reached and then descending order, and then repeat as many times as second parameter number. The star series should have a space inbetween each series. ", function () {
 
     outputData = ""
     const num = 4
@@ -31,16 +19,4 @@ describe("exercise3", () => {
     expect(expectedResult == outputData || otherResult == outputData).toBeTruthy()
 })
 
-    test("Should console.log stars on separate lines in ascending order until first paramater number of stars is reached and then descending order, and then repeat as many times as second parameter number ", function () {
-
-    outputData = ""
-    const num = 3
-    const count = 3
-    const expectedResult = "*~**~***~**~*~~*~**~***~**~*~~*~**~***~**~*~~"
-    const otherResult = "*\n**\n***\n**\n*\n~*\n**\n***\n**\n*\n~*\n**\n***\n**\n*\n~"
-
-
-    allFuncs.printStarSeries(num, count)
-    expect(expectedResult == outputData || otherResult == outputData).toBeTruthy()
-})
 })

2f4b1f2ea8e6a36150e06e4c97a2f0d7921b2a65
Corinne <corinnegottlieb@gmail.com>
2019-09-10 11:13:56 +0300

    ex5 edit [ci skip]

--- exercises/ex-5.test.js
+++ exercises/ex-5.test.js
@@ -1,23 +1,19 @@
 const allFuncs = require("../../src/App")
 
 describe("exercise5", () => {
-    it('Output should read the same backwards as it does forwards', () => {
+    it('Output should read the same backwards as it does forwards (ignoring spaces)', () => {
         let input = "Taco cat"
         let expectedOutput = true
-        expect(allFuncs.isPallindrome(input)).toBe(expectedOutput)
-        
-      });
-      it('Output should read the same backwards as it does forwards', () => {
-        let input = "shoe"
-        let expectedOutput = false
-        expect(allFuncs.isPallindrome(input)).toBe(expectedOutput)
-        
-      });       
-      it('Emtpy string paramater should return empty string', () => {
-        let input = ""
-        let expectedOutput = true
-        expect(allFuncs.isPallindrome(input)).toBe(expectedOutput)
+        expect(allFuncs.isPallindrome(input), `When '${input}' enters the function, ${expectedOutput} should be returned`).toBe(expectedOutput)
+        let input2 = "shoe"
+        let expectedOutput2 = false
+        expect(allFuncs.isPallindrome(input2), `When '${input2}' enters the function, ${expectedOutput2} should be returned`).toBe(expectedOutput2)
+        let input3 = ""
+        let expectedOutput3 = true
+        expect(allFuncs.isPallindrome(input3), 'Emtpy string paramater should return empty string').toBe(expectedOutput3)
      
+       
       });
+  
     })
       

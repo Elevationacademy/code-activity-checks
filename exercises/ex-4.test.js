b373a4daa5d5b5ae5996fcdaaf1b69d037827fb6
Corinne <corinnegottlieb@gmail.com>
2019-09-10 12:02:01 +0300

    testing

--- exercises/ex-4.test.js
+++ exercises/ex-4.test.js
@@ -4,17 +4,21 @@ describe("exercise4", () => {
   it('The function should return the string reversed', () => {
     let input1 = "shoe"
     let expectedOutput1 = "eohs"
-let result = allFuncs.reverse(input1)
+let result1 = allFuncs.reverse(input1)
 
-    expect(allFuncs.reverse(input1), `Output string should be the input string reversed. If the string '${input1}' enters the function, the function should return the string '${expectedOutput1}', instead returned '${result}'.`).toBe(expectedOutput1)
+    expect(allFuncs.reverse(input1), `Output string should be the input string reversed. If the string '${input1}' enters the function, the function should return the string '${expectedOutput1}', instead the function returned: '${result1}'`).toBe(expectedOutput1)
     
     let input2 = "race car"
     let expectedOutput2 = "rac ecar"
-    expect(allFuncs.reverse(input2), `String should be reversed including the space. If the string '${input2}' enters the function, the function should return the string '${expectedOutput2}'.`).toBe(expectedOutput2)
+    let result2 = allFuncs.reverse(input2)
+
+    expect(allFuncs.reverse(input2), `String should be reversed including the space. If the string '${input2}' enters the function, the function should return the string '${expectedOutput2}', instead the function returned: '${result2}'`).toBe(expectedOutput2)
     
     let input3 = ""
     let expectedOutput3 = ""
-    expect(allFuncs.reverse(input3), `An emtpy string paramater should return empty string. If the string '${input3}' enters the function, the function should return '${expectedOutput3}'.`).toBe(expectedOutput3)
+    let result3 = allFuncs.reverse(input3)
+
+    expect(allFuncs.reverse(input3), `An emtpy string paramater should return empty string. If the string '${input3}' enters the function, the function should return '${expectedOutput3}', instead the function returned: '${result3}'`).toBe(expectedOutput3)
   });
 })
 
--- exercises/ex-5.test.js
+++ exercises/ex-5.test.js
@@ -4,16 +4,20 @@ describe("exercise5", () => {
     it('Output should read the same backwards as it does forwards (ignoring spaces)', () => {
         let input1 = "Taco cat"
         let expectedOutput1 = true
-        let result = allFuncs.isPallindrome(input1)
-        expect(allFuncs.isPallindrome(input1), `If the string '${input1}' enters the function, the function should return the string '${expectedOutput1}', instead returned '${result}'.`).toBe(expectedOutput)
+        let result1 = allFuncs.isPallindrome(input1)
+        expect(allFuncs.isPallindrome(input1), `If the string '${input1}' enters the function, the function should return ${expectedOutput1}, instead the function returned: ${result1}`).toBe(expectedOutput1)
        
         let input2 = "shoe"
         let expectedOutput2 = false
-        expect(allFuncs.isPallindrome(input2), `When '${input2}' enters the function, ${expectedOutput2} should be returned`).toBe(expectedOutput2)
+        let result2 = allFuncs.isPallindrome(input2)
+
+        expect(allFuncs.isPallindrome(input2), `If the string '${input2}' enters the function, the function should return ${expectedOutput2}, instead the function returned: ${result2}`).toBe(expectedOutput2)
        
         let input3 = ""
         let expectedOutput3 = true
-        expect(allFuncs.isPallindrome(input3), 'Emtpy string paramater should return empty string').toBe(expectedOutput3)
+        let result3 = allFuncs.isPallindrome(input3)
+
+        expect(allFuncs.isPallindrome(input3), `Empty string paramater should return ${expectedOutput3}, instead the function returned: ${result3} `).toBe(expectedOutput3)
      
        
       });

2df37cfce8e7b6c6853d269740351219e77e424a
Corinne <corinnegottlieb@gmail.com>
2019-09-10 19:15:13 +0300

    edit ex9

--- exercises/ex-2.test.js
+++ exercises/ex-2.test.js
@@ -7,12 +7,12 @@ console["log"] = jest.fn(storeLog)
 describe("exercise2", () => {
     test("Should console.log stars on separate lines in descending order with increments of one (starting with parameter number of stars until one star", function () {
     
-    outputData = ""
+    
     const num = 3
     const expectedResult = "***~**~*~"
     const otherResult = "***\n**\n*\n~"
 
     allFuncs.printBackwardsStars(num)
-    expect(expectedResult == outputData || otherResult == outputData, `If num = 3, function should console.log ${expectedResult} or ${otherResult}).toBeTruthy()
+    expect(expectedResult == outputData || otherResult == outputData, `If num = 3, function should console.log ${expectedResult} or ${otherResult}`).toBeTruthy()
 })
 })
--- exercises/ex-8.test.js
+++ exercises/ex-8.test.js
@@ -5,23 +5,25 @@ describe("exercise8", () => {
     let arr1 = ["red", "indigo", "white", "teal", "yellow"]
     let arr2 = ["bread", "cheese", "cucumber"];
     let length = arr1.length + arr2.length
-    expect(allFuncs.jumble(arr1, arr2)).toHaveLength(length)
+    expect(allFuncs.jumble(arr1, arr2), `the length of the jumbled array was not equal to the sums of the two array lengths`).toHaveLength(length)
   });
-  it('The jumbled array should not be in the exact same order as the two arrays', () => {
+  it('The jumbled array should be in a random order and not the exact same order as the two arrays', () => {
     let arr1 = ["red", "indigo", "white", "teal", "yellow"]
     let arr2 = ["bread", "cheese", "cucumber"];
     let combinedArr = arr1.concat(arr2)
-    expect(allFuncs.jumble(arr1, arr2)).not.toBe(combinedArr)
+    expect(allFuncs.jumble(arr1, arr2), 'the jumbled array should not be just the two arrays concatenated').not.toBe(combinedArr)
+    expect(allFuncs.jumble(arr2, arr1), 'the jumbled array should not be just the two arrays concatenated').not.toBe(combinedArr)
+
   });
   it('The jumbled array should contain all elements from array 1', () => {
     let arr1 = ["red", "indigo", "white", "teal", "yellow"]
     let arr2 = ["bread", "cheese", "cucumber"];
-    expect(allFuncs.jumble(arr1, arr2)).toEqual(expect.arrayContaining(arr1))
+    expect(allFuncs.jumble(arr1, arr2), 'could not find all the items from the first parameter in the jumbled array').toEqual(expect.arrayContaining(arr1))
   });
   it('The jumbled array should contain all elements from array2', () => {
     let arr1 = ["red", "indigo", "white", "teal", "yellow"]
     let arr2 = ["bread", "cheese", "cucumber"];
-    expect(allFuncs.jumble(arr1, arr2)).toEqual(expect.arrayContaining(arr2))
+    expect(allFuncs.jumble(arr1, arr2), 'could not find all the items from the second parameter in the jumbled array').toEqual(expect.arrayContaining(arr2))
   });
 
 })

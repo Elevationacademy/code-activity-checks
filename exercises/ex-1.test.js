c2dc68aaf6e164a47e8893cfb302a36de6f35d3c
danbrud <dannybrudner@users.noreply.github.com>
2019-12-19 13:38:43 +0200

    added jest-puppeteer

--- exercises/ex-1.test.js
+++ exercises/ex-1.test.js
@@ -1,4 +1,3 @@
-const puppeteer = require('puppeteer')
 const $ = require('cheerio')
 const path = require('path')
 
@@ -7,30 +6,30 @@ const selectElements = function (html) {
   return firstPost
 }
 
-const getHtml = async function () {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  console.log('file://' + path.join(__dirname, '..', '..', 'src', 'index.html'))
-  await page.goto('file://' + path.join(__dirname, '..', '..', 'src', 'index.html'))
+// const getHtml = async function () {
+//   await page.goto('file://' + path.join(__dirname, '..', '..', 'src', 'index.html'))
 
-  const html = await page.content()
-  return html
-}
+//   const html = await page.content()
+//   return html
+// }
 
-beforeAll(done => {
-  done()
-})
 
 describe('exercise-1', () => {
+  beforeAll(async () => {
+    await page.goto('file://' + path.join(__dirname, '..', '..', 'src', 'index.html'))
+  })
+
   it('Should create an `enqueue` method which adds a new element to the end of the queue', async function (done) {
-    const html = await getHtml()
+    const html = await page.content()
     const id = selectElements(html)['data-id']
+    // const firstPost = page.$('.todo')[0].attribs['data-id']
+
 
     expect(id, `when enqueueing 1 to the queue, the length of the queue should be 1`).toBe('0')
     done()
   })
-})
 
-afterAll(async done => {
-  done()
+  afterAll(async done => {
+    done()
+  })
 })

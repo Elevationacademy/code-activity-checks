4141258879526f47c09d9e053dba7645a162091c
danbrud <dannybrudner@users.noreply.github.com>
2020-02-20 16:56:32 +0200

    added tests

---
+++ exercises/ex-2.test.js
@@ -0,0 +1,12 @@
+const { migrateData } = require('../../src/app')
+const utils = require('../../utils/utils.class')
+
+describe('Exercise 2', function () {
+    it('The function findOddOccurrences should find all the numbers that appear an odd number of times and output them in an array', function () {
+        const movies = require('../../utils/dummyMovies.json')
+        const expectedResult = require('../../utils/migratedMovies.json')
+        const result = migrateData(movies)
+
+        expect(result, `the function did not return the correct object - when passing ${JSON.stringify(movies)} to the function expected ${JSON.stringify(expectedResult)} but instead got ${JSON.stringify(result)}. We suggest you parse these stringified objects in order to comfortably see how they look`).toEqual(expectedResult)
+    })
+} )

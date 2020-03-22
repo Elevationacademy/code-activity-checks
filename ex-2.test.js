24acf170854695f6db8362db4c5a794514457455
danbrud <dannybrudner@users.noreply.github.com>
2020-03-22 18:50:01 +0200

    added tests

---
+++ ex-2.test.js
@@ -0,0 +1,47 @@
+const $ = require('cheerio')
+const path = require('path')
+
+describe('exercise-2', () => {
+  beforeAll(async () => {
+    await page.goto('file://' + path.join(__dirname, '..', '..', 'src', 'index.html'))
+  })
+
+  it(`If the user enters an invalid breed, you should append a 'p' element to the 'div' with id 'result' letting the user know the breed was 'not found'.`, async function (done) {
+
+    let html = await page.content()
+    const pElem = $('#result p', html)
+    expect(pElem.length, `There was a 'p' element in the 'div' with id 'result' before initiating the search. Make sure to make the API request only when the user clicks the search button and append the error message only when there is an error`).toBe(0)
+
+    const searchValue = 'h'
+    await page.evaluate((searchValue) => {
+      document.querySelector('#breed-input').value = searchValue
+    }, searchValue)
+    await page.click('#search')
+    await page.waitFor(4000)
+    html = await page.content()
+
+    let text
+    try {
+      text = $('#result p', html).text().toLowerCase()
+    } catch (e) {
+      text = null
+    }
+
+    if (text) {
+      let includesErrorText = false
+      if (text.includes('not') && text.includes('found')) {
+        includesErrorText = true
+      }
+
+      expect(includesErrorText, `The 'p' element did not have an error message inside when searching for an invalid input such as '${searchValue}', make sure to add a 'p' element with the message "Breed not found - try again" to the 'div' with id 'result' if there is an 'error' with the API request`).toBeTruthy()
+      done()
+    } else {
+      expect(false, `There was no 'p' in the 'div' with id 'result' after the API request was made. Please append a 'p' with the correct text to the 'result' 'div' after the error occurs`).toBeTruthy()
+      done()
+    }
+  })
+
+  afterAll(async done => {
+    done()
+  })
+})
---
+++ exercises/ex-1.test.js
@@ -0,0 +1,49 @@
+const $ = require('cheerio')
+const path = require('path')
+
+describe('exercise-1', () => {
+  beforeAll(async () => {
+    await page.goto('file://' + path.join(__dirname, '..', '..', 'src', 'index.html'))
+  })
+
+  it(`When clicking on the 'Search' button, you should make an API request to the Dog API with the breed from the input`, async function (done) {
+    let html = await page.content()
+    const imgElem = $('#result img', html)
+    expect(imgElem.length, `There was an image in the 'div' with id 'result' before initiating the search. Make sure to make the API request only when the user clicks the search button and append the image when the response from the API is received`).toBe(0)
+
+    const searchValue = 'hound'
+    await page.evaluate((searchValue) => {
+      document.querySelector('#breed-input').value = searchValue
+    }, searchValue)
+    await page.click('#search')
+    await page.waitFor(4000)
+    html = await page.content()
+
+    let src
+    try {
+      src = $('#result img', html).attr('src')
+    } catch (e) {
+      src = null
+    }
+
+    if (src) {
+      let isImage = false
+      const imageExtensions = ['.jpg', '.jpeg', 'png']
+      imageExtensions.forEach(e => {
+        if (src.toLowerCase().includes(e)) {
+          isImage = true
+        }
+      })
+
+      expect(isImage, `The 'src' attribute of the 'img' tag was not a valid image when searching for '${searchValue}', make sure to add the image link from the API response to the 'src' attribute of the 'img'`).toBeTruthy()
+      done()
+    } else {
+      expect(false, `There was no image in the 'div' with id 'result' after the API request was made. Please append an image with the correct 'src' attribute to the 'result' 'div' after the response from the API is received`).toBeTruthy()
+      done()
+    }
+  })
+
+  afterAll(async done => {
+    done()
+  })
+})

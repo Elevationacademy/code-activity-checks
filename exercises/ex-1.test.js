c39aca7d87dd76f9b01fd34a892d2163fe5778f7
danbrud <dannybrudner@users.noreply.github.com>
2020-04-02 14:28:54 +0300

    modified error handling

--- exercises/ex-1.test.js
+++ exercises/ex-1.test.js
@@ -3,21 +3,14 @@ const path = require('path')
 
 describe('exercise-1', () => {
   beforeAll(async () => {
-    jest.setTimeout(20000)
+    jest.setTimeout(30000)
+    await jestPuppeteer.resetBrowser()
     await page.goto('file://' + path.join(__dirname, '..', '..', 'src', 'index.html'))
   })
 
   it(`When clicking on the 'Search' button, you should make an API request to the Dog API with the breed from the input`, async function (done) {
-    let html, imgElem
-
-    try {
-      html = await page.content()
-      imgElem = $('#result img', html)
-    } catch (e) {
-      expect(false, `Hmm, seems the code you submitted is crashing. Please check things like syntax and try again.`).toBeTruthy()
-      done()
-      return
-    }
+    let html = await page.content()
+    let imgElem = $('#result img', html)
     expect(imgElem.length, `There was an image in the 'div' with id 'result' before initiating the search. Make sure to make the API request only when the user clicks the search button and append the image when the response from the API is received`).toBe(0)
 
     let searchValue
@@ -30,37 +23,29 @@ describe('exercise-1', () => {
       await page.waitFor(4000)
       html = await page.content()
     } catch (e) {
-      console.log(e.message)
-      console.log(e.stack)
       expect(false, `Hmm, seems the code you submitted is crashing. Please check things like syntax and try again.`).toBeTruthy()
-      done()
-      return
     }
 
     let src
     try {
-      src = $('#result img', html).attr('src')
+      src = $('#result img', html).attr('src').toLowerCase()
     } catch (e) {
-      expect(false, `There was no image in the 'div' with id 'result' after the API request was made. Please append an image with the correct 'src' attribute to the 'result' 'div' after the response from the API is received`).toBeTruthy()
-      done()
-      return
+      src = null
     }
 
     if (src) {
       let isImage = false
       const imageExtensions = ['.jpg', '.jpeg', 'png']
       imageExtensions.forEach(e => {
-        if (src.toLowerCase().includes(e)) {
-          isImage = true
-        }
+        if (src.includes(e)) { isImage = true }
       })
 
       expect(isImage, `The 'src' attribute of the 'img' tag was not a valid image when searching for '${searchValue}', make sure to add the image link from the API response to the 'src' attribute of the 'img'`).toBeTruthy()
       done()
-      return
+    } else {
+      expect(false, `There was no image in the 'div' with id 'result' after the API request was made. Please append an image with the correct 'src' attribute to the 'result' 'div' after the response from the API is received`).toBeTruthy()
+      done()
     }
-
-    done()
   })
 
   afterAll(async done => {
--- exercises/ex-2.test.js
+++ exercises/ex-2.test.js
@@ -3,19 +3,13 @@ const path = require('path')
 
 describe('exercise-2', () => {
   beforeAll(async () => {
-    jest.setTimeout(20000)
+    jest.setTimeout(30000)
     await page.goto('file://' + path.join(__dirname, '..', '..', 'src', 'index.html'))
   })
 
   it(`If the user enters an invalid breed, you should append a 'p' element to the 'div' with id 'result' letting the user know the breed was 'not found'.`, async function (done) {
-    let html, pElem
-
-    try {
-      html = await page.content()
-      pElem = $('#result p', html)
-    } catch (e) {
-      expect(false, `Hmm, seems the code you submitted is crashing. Please check things like syntax and try again.`).toBeTruthy()
-    }
+    let html = await page.content()
+    let pElem = $('#result p', html)
     expect(pElem.length, `There was a 'p' element in the 'div' with id 'result' before initiating the search. Make sure to make the API request only when the user clicks the search button and append the error message only when there is an error`).toBe(0)
 
     let searchValue
@@ -36,7 +30,6 @@ describe('exercise-2', () => {
       text = $('#result p', html).text().toLowerCase()
     } catch (e) {
       text = null
-      expect(false, `There was no 'p' in the 'div' with id 'result' after the API request was made. Please append a 'p' with the correct text to the 'result' 'div' after the error occurs`).toBeTruthy()
     }
 
     if (text) {
@@ -44,9 +37,11 @@ describe('exercise-2', () => {
       if (text.includes('not') && text.includes('found')) { includesErrorText = true }
 
       expect(includesErrorText, `The 'p' element did not have an error message inside when searching for an invalid input such as '${searchValue}', make sure to add a 'p' element with the message "Breed not found - try again" to the 'div' with id 'result' if there is an 'error' with the API request`).toBeTruthy()
+      done()
+    } else {
+      expect(false, `There was no 'p' in the 'div' with id 'result' after the API request was made. Please append a 'p' with the correct text to the 'result' 'div' after the error occurs`).toBeTruthy()
+      done()
     }
-
-    done()
   })
 
   afterAll(async done => {

a0f4502da2f9bbe31cbe584eca0b62dadc92f165
danbrud <dannybrudner@users.noreply.github.com>
2020-04-20 16:47:09 +0300

    added error handling to ex 2

--- exercises/ex-2.test.js
+++ exercises/ex-2.test.js
@@ -1,5 +1,4 @@
 const client = require('../../utils/client.class')
-const dbUtils = require('../../utils/db.class')
 
 describe('exercise2', () => {
   let server
@@ -11,13 +10,34 @@ describe('exercise2', () => {
 
   it('You should write a query to your db from the `/users` route to save the given user and res.send() the saved user in the response', async done => {
     const user = { username: 'shoobidoobi', age: 88 }
-    await client.addDoc(user, 'users')
 
-    const newUser = await dbUtils.queryDB('User', { username: user.username })
+    let hasError
+    try {
+      await client.addDoc(user, 'users')
+    } catch (e) {
+      hasError = true
+    }
 
-    expect(newUser.username, `After posting ${JSON.stringify(user)} to your /users route and querying the db, username should be ${user.username} instead received ${newUser.username}`).toBe(user.username)
-    expect(newUser.age, `After posting ${JSON.stringify(user)} to your /users route and querying the db, age should be ${user.age} instead received ${newUser.age}`).toBe(user.age)
-    expect(newUser.messages.length, `After posting ${JSON.stringify(user)} to your /users route and querying the db, messages should be an empty array instead the array had length of ${newUser.messages.length}`).toBe(0)
+    if (hasError) {
+      expect(false, 'Hmm, seems the code you submitted is crashing. Please check things like syntax and try again.').toBeTruthy()
+    } else {
+      let newUser
+      hasError = false
+      try {
+        const dbUtils = require('../../utils/db.class')
+        newUser = await dbUtils.queryDB('User', { username: user.username })
+      } catch (e) {
+        hasError = true
+      }
+
+      if (hasError) {
+        expect(false, "There seems to be something wrong with the export of your model/s, check them and try again").toBeTruthy()
+      } else {
+        expect(newUser.username, `After posting ${JSON.stringify(user)} to your /users route and querying the db, username should be ${user.username} instead received ${newUser.username}`).toBe(user.username)
+        expect(newUser.age, `After posting ${JSON.stringify(user)} to your /users route and querying the db, age should be ${user.age} instead received ${newUser.age}`).toBe(user.age)
+        expect(newUser.messages.length, `After posting ${JSON.stringify(user)} to your /users route and querying the db, messages should be an empty array instead the array had length of ${newUser.messages.length}`).toBe(0)
+      }
+    }
     done()
   })
 

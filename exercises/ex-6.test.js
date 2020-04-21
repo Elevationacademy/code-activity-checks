38674dbe752212884901a728d9a56a4e05af82ad
danbrud <dannybrudner@users.noreply.github.com>
2020-04-21 13:21:45 +0300

    error handling ex 6

--- exercises/ex-6.test.js
+++ exercises/ex-6.test.js
@@ -3,25 +3,44 @@ const dbUtils = require('../../utils/db.class');
 const client = require('../../utils/client.class');
 
 describe('exercise6', () => {
-  let server;
+  let server, dbUtils, hasError
   beforeAll(async done => {
-    server = require('../../src/server');
-    await dbUtils.dropDBs();
-    done();
+    server = require('../../src/server')
+    try {
+      dbUtils = require('../../utils/db.class')
+      await dbUtils.dropDBs()
+    } catch (e) {
+      hasError = true
+    }
+    done()
   });
 
   it('You should write a query to your db from the `/users/:username` to find a user by a given username and res.send() this user with his extracted messages (using populate)', async done => {
-    const dummyUsers = require('../../utils/dummyUsers.json');
-    await dbUtils.addToDB('User', dummyUsers[0]);
+    if (hasError) {
+      expect(false, "There seems to be something wrong with the export of your model/s, check them and try again").toBeTruthy()
+    } else {
+      const dummyUsers = require('../../utils/dummyUsers.json');
+      await dbUtils.addToDB('User', dummyUsers[0]);
+      let user = await dbUtils.queryDB('User', { username: dummyUsers[0].username });
+      const messageToSave = { text: 'Hello there', time: new Date() };
 
-    let user = await dbUtils.queryDB('User', { username: dummyUsers[0].username });
-    const messageToSave = { text: 'Hello there', time: new Date() };
-    const returnedMessage = await client.addDoc(messageToSave, `messages/${user._id}`);
-    const messageFound = await dbUtils.queryDB('Message', { text: messageToSave.text });
-    const populatedUser = await client.getPopulatedUser(dummyUsers[0].username);
-    user = await dbUtils.queryDB('User', { username: user.username })
+      let returnedMessage, messageFound, populatedUser
+      hasError = false
+      try {
+        returnedMessage = await client.addDoc(messageToSave, `messages/${user._id}`);
+        messageFound = await dbUtils.queryDB('Message', { text: messageToSave.text });
+        populatedUser = await client.getPopulatedUser(dummyUsers[0].username);
+        user = await dbUtils.queryDB('User', { username: user.username })
+      } catch (e) {
+        hasError = false
+      }
 
-    expect(populatedUser.messages[0].text, `After adding a message with id ${messageFound._id} and text of '${messageToSave.text}' to the db for a user with id ${user._id} and querying the db for that user with his populated messages, the user's message array didn't include the populated message. The user's message array looked like this: ${JSON.stringify(user.messages)}, while it should look like this: [{"_id": "${messageFound._id}", "text": "${messageToSave.text}", "time": "${messageToSave.time}"}]`).toBe(messageToSave.text);
+      if (hasError) {
+        expect(false, 'Hmm, seems the code you submitted is crashing. Please check things like syntax and try again.').toBeTruthy()
+      } else {
+        expect(populatedUser.messages[0].text, `After adding a message with id ${messageFound._id} and text of '${messageToSave.text}' to the db for a user with id ${user._id} and querying the db for that user with his populated messages, the user's message array didn't include the populated message. The user's message array looked like this: ${JSON.stringify(user.messages)}, while it should look like this: [{"_id": "${messageFound._id}", "text": "${messageToSave.text}", "time": "${messageToSave.time}"}]`).toBe(messageToSave.text);
+      }
+    }
     done();
   });
 

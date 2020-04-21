4ebf242630b913a8599ca35ba9c082b1c27ed60a
Lior Hoshea <hoshealior@gmail.com>
2020-04-21 17:19:27 +0300

    fixed Player test

--- exercises/ex-4.test.js
+++ exercises/ex-4.test.js
@@ -10,9 +10,20 @@ configure({ adapter: new Adapter() });
 describe('exercise4', () => {
   it('The Player component should have 2 td tags inside the tr tag: the first one with the name attribute and second with the score attribute (both passed from LeaderBoard as props)', () => {
     const p = { id: 1, name: 'Jona', score: '100' };
-    let wrapper, hasError;
+    let wrapper;
     try {
       wrapper = shallow(<Player key={p.id} name={p.name} score={p.score} />);
+    } catch (e) {
+      expect(
+        false,
+        'Hmm, seems the code you submitted is crashing. Please check things like syntax and try again.'
+      ).toBeTruthy();
+    }
+
+    let name, score, hasError;
+    try {
+      name = wrapper.props().children[0];
+      score = wrapper.props().children[1];
     } catch (e) {
       hasError = true;
     }
@@ -20,15 +31,15 @@ describe('exercise4', () => {
     if (hasError) {
       expect(
         false,
-        'Hmm, seems the code you submitted is crashing. Please check things like syntax and try again.'
+        'Could not find any properties for the Player component. Make sure you render the wanted <td>s with name and score of the player'
       ).toBeTruthy();
     } else {
       expect(
-        wrapper.props().children[0],
+        name,
         `When passing name attribute with the value of 'Jona', the first child tag should be - <td>Jona</td>. Make sure that the first td tag inside the tr tag is the player's name, and that you're passing the name property from LeaderBoard to Player, using props`
       ).toEqual(<td>Jona</td>);
       expect(
-        wrapper.props().children[1],
+        score,
         `When passing score attribute with the value of 100, the second child tag should be - <td>100</td>. Make sure that the second td tag inside the tr tag is the player's score, and that you're passing the score property from LeaderBoard to Player, using props`
       ).toEqual(<td>100</td>);
     }

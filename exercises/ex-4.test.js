f4e8164f42bdff338dfce14de71b8122f7e3becc
danbrud <dannybrudner@users.noreply.github.com>
2020-05-03 16:27:47 +0300

    modified expect messages for ex 4

--- exercises/ex-4.test.js
+++ exercises/ex-4.test.js
@@ -8,7 +8,7 @@ configure({ adapter: new Adapter() });
 
 // Player component test
 describe('exercise4', () => {
-  it('The Player component should have 2 td tags inside the tr tag: the first one with the name attribute and second with the score attribute (both passed from LeaderBoard as props)', () => {
+  it('The Player component should have 2 td tags inside the tr tag: the first one with the player\'s name and the second with the player\'s score (both passed from LeaderBoard as props)', () => {
     const p = { id: 1, name: 'Jona', score: '100' };
     let wrapper;
     try {
@@ -31,16 +31,16 @@ describe('exercise4', () => {
     if (hasError) {
       expect(
         false,
-        'Could not find any properties for the Player component. Make sure you render the wanted <td>s with name and score of the player'
+        'Could not find any properties for the Player component. Make sure you render the required <td> tags with the name and score of the player'
       ).toBeTruthy();
     } else {
       expect(
         name,
-        `When passing name attribute with the value of 'Jona', the first child tag should be - <td>Jona</td>. Make sure that the first td tag inside the tr tag is the player's name, and that you're passing the name property from LeaderBoard to Player, using props`
+        `When rendering a player with the name 'Jona', the first child tag should be - <td>Jona</td>. Make sure that the first td tag inside the tr tag is the player's name, and that you're passing the name property from LeaderBoard to Player using props`
       ).toEqual(<td>Jona</td>);
       expect(
         score,
-        `When passing score attribute with the value of 100, the second child tag should be - <td>100</td>. Make sure that the second td tag inside the tr tag is the player's score, and that you're passing the score property from LeaderBoard to Player, using props`
+        `When rendering a player with the score 100, the second child tag should be - <td>100</td>. Make sure that the second td tag inside the tr tag is the player's score, and that you're passing the score property from LeaderBoard to Player using props`
       ).toEqual(<td>100</td>);
     }
   });

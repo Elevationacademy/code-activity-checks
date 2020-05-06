229236991f12786938ea05859e738b75b0115dcd
Lior Hoshea <hoshealior@gmail.com>
2020-05-06 13:25:14 +0300

    fixed tests

--- exercises/ex-1.test.js
+++ exercises/ex-1.test.js
@@ -1,40 +1,28 @@
 describe('Exercise 1', function () {
   it('findMoviesByDurRange function should receive 2 parameters (minDur and maxDur) and return an array with titles of movies which duration is between minDur and maxDur (not included)', function () {
-    let findMoviesByDurRange, hasError;
     try {
-      findMoviesByDurRange = require('../../src/exercise1');
-    } catch (e) {
-      hasError = true;
-    }
-
-    if (hasError) {
-      expect(
-        false,
-        'Hmm, seems the code you submitted is crashing when trying to import findMoviesByDurRange function. Please make sure you are using the correct function name and try again.'
-      ).toBeTruthy();
-    } else {
+      let findMoviesByDurRange = require('../../src/exercise1');
       expect(
         findMoviesByDurRange(80, 90),
-        `Expected to receive ['Madagascar: Escape 2 Africa', 'Madagascar', 'Big Nothing', 'Ice Age'] when searching for movies between 80 to 90 minutes, but received ${findMoviesByDurRange(
-          80,
-          90
-        )} instead`
+        `Expected to receive ["Madagascar: Escape 2 Africa", "Madagascar", "Big Nothing", "Ice Age"] when searching for movies between 80 to 90 minutes, but received ${JSON.stringify(
+          findMoviesByDurRange(80, 90)
+        )} instead. Make sure the order of the given parameters is correct, and that you return an array with the matching movies' titles`
       ).toEqual(['Madagascar: Escape 2 Africa', 'Madagascar', 'Big Nothing', 'Ice Age']);
 
       expect(
         findMoviesByDurRange(0, 60),
-        `Expected to receive [] when searching for movies between 0 to 60 minutes, but received ${findMoviesByDurRange(
+        `Expected to receive [] when searching for movies between 0 to 60 minutes, but received ${JSON.stringify(findMoviesByDurRange(
           0,
           60
-        )} instead`
+        ))} instead. Make sure the order of the given parameters is correct, and that you return an array with the matching movies' titles`
       ).toEqual([]);
 
       expect(
         findMoviesByDurRange(118, 120),
-        `Expected to receive ['Looper', 'The Beach', 'Match Point', 'American History X','Birdman or (The Unexpected Virtue of Ignorance)'] when searching for movies between 118 to 120 minutes, but received ${findMoviesByDurRange(
+        `Expected to receive ["Looper", "The Beach", "Match Point", "American History X","Birdman or (The Unexpected Virtue of Ignorance)"] when searching for movies between 118 to 120 minutes, but received ${JSON.stringify(findMoviesByDurRange(
           118,
           120
-        )} instead`
+        ))} instead. Make sure the order of the given parameters is correct, and that you return an array with the matching movies' titles`
       ).toEqual([
         'Looper',
         'The Beach',
@@ -42,6 +30,8 @@ describe('Exercise 1', function () {
         'American History X',
         'Birdman or (The Unexpected Virtue of Ignorance)',
       ]);
+    } catch (error) {
+      expect(false, `${error}`).toBeTruthy();
     }
   });
 });
--- exercises/ex-2.test.js
+++ exercises/ex-2.test.js
@@ -1,41 +1,29 @@
 describe('Exercise 2', function () {
   it('findMoviesByYearAndGenre function should receive 2 parameters (year and genre) and return an array with titles of movies with matching year and genre', function () {
-    let findMoviesByYearAndGenre, hasError;
     try {
-      findMoviesByYearAndGenre = require('../../src/exercise2');
-    } catch (e) {
-      hasError = true;
-    }
-
-    if (hasError) {
-      expect(
-        false,
-        'Hmm, seems the code you submitted is crashing when trying to import findMoviesByYearAndGenre function. Please make sure you are using the correct function name and try again.'
-      ).toBeTruthy();
-    } else {
+      let findMoviesByYearAndGenre = require('../../src/exercise2');
       expect(
         findMoviesByYearAndGenre(2002, 'Animation'),
-        `Expected to receive ['Ice Age'] when searching for movies with year of 2002 and genre of Animation, but received ${findMoviesByYearAndGenre(
-          2002,
-          'Animation'
-        )} instead`
+        `Expected to receive ["Ice Age"] when searching for movies with year of 2002 and genre of Animation, but received ${JSON.stringify(
+          findMoviesByYearAndGenre(2002, 'Animation')
+        )} instead. Make sure the order of the given parameters is correct, and that you return an array with the matching movies' titles`
       ).toEqual(['Ice Age']);
 
       expect(
         findMoviesByYearAndGenre(2003, 'History'),
-        `Expected to receive ['The Last Samurai'] when searching for movies with year of 2003 and genre of History, but received ${findMoviesByYearAndGenre(
-          2003,
-          'History'
-        )} instead`
+        `Expected to receive ["The Last Samurai"] when searching for movies with year of 2003 and genre of History, but received ${JSON.stringify(
+          findMoviesByYearAndGenre(2003, 'History')
+        )} instead. Make sure the order of the given parameters is correct, and that you return an array with the matching movies' titles`
       ).toEqual(['The Last Samurai']);
 
       expect(
-        findMoviesByYearAndGenre(1974, 'Mystery'),
-        `Expected to receive ['Chinatown'] when searching for movies with year of 2003 and genre of History, but received ${findMoviesByYearAndGenre(
-          1974,
-          'Mystery'
-        )} instead`
-      ).toEqual(['Chinatown']);
+        findMoviesByYearAndGenre(2007, 'Family'),
+        `Expected to receive ["Ratatouille", "Stardust"] when searching for movies with year of 2007 and genre of Family, but received ${JSON.stringify(
+          findMoviesByYearAndGenre(2007, 'Family')
+        )} instead. Make sure the order of the given parameters is correct, and that you return an array with the matching movies' titles`
+      ).toEqual(['Ratatouille', 'Stardust']);
+    } catch (error) {
+      expect(false, `${error}`).toBeTruthy();
     }
   });
 });

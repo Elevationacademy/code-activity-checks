d3bc744206a1012ddd57c1be74ec982fb91beb97
danbrud <dannybrudner@users.noreply.github.com>
2020-05-20 18:49:28 +0300

    modified language for ex3-4

--- exercises/ex-3.test.js
+++ exercises/ex-3.test.js
@@ -8,50 +8,50 @@ describe('Exercise 3', () => {
     await page.goto('file://' + path.join(__dirname, '..', '..', 'src', 'index.html'));
   });
 
-  it(`index.html should not be modified`, async function (done) {
+  it(`The index.html file should not be modified`, async function (done) {
     const htmlContent = fs.readFileSync(
       path.join(__dirname, '..', '..', 'src', 'index.html'),
       'utf8'
     );
     expect(
       htmlContent.includes('<h1>'),
-      `index.html contain an h1 element, although it should not be modified. Reminder: you should not add any elements to the index.html file ; make sure you write your code only in the main.js file`
+      `The index.html file contains an h1 element, although it should not be modified. Reminder: you should not add any elements to the index.html file - make sure you write your code only in the main.js file`
     ).toBeFalsy();
     expect(
       htmlContent.includes('class="team-member"'),
-      `index.html contain an element with class of 'team-member', although it should not be modified. Reminder: you should not add any elements to the index.html file ; make sure you write your code only in the main.js file`
+      `The index.html file contain an element with class of 'team-member', although it should not be modified. Reminder: you should not add any elements to the index.html file - make sure you write your code only in the main.js file`
     ).toBeFalsy();
     done();
   });
 
-  it(`Should use jQuery and not DOM commands`, async function (done) {
+  it(`You should use jQuery and not vanilla JS DOM methods`, async function (done) {
     const mainContent = fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'main.js'), 'utf8');
     expect(
       mainContent.includes('$'),
-      `Could not find '$' (jQuery) in the main.js file. Rememeber that you should use jQuery and not DOM commands`
+      `Could not find '$' (jQuery) in the main.js file. Rememeber that you should use jQuery and not vanilla JS DOM operations`
     ).toBeTruthy();
     expect(
       mainContent.includes('closest'),
-      `Could not find 'closest' keyword in main.js file. Rememeber that you should use jQuery and not DOM commands`
+      `Could not find the 'closest' keyword in main.js file. Rememeber that you should use jQuery and not vanilla JS DOM operations`
     ).toBeTruthy();
     done();
   });
 
-  it(`Should display a h1 tag with 'The Team:' as innerHTML after clicking on the about button (span)`, async function (done) {
+  it(`Clicking on the 'about' button (span) should display an 'h1' tag with 'The Team:' as it's innerHTML`, async function (done) {
     try {
       await page.click('#about');
       let html = await page.content();
       let header = $('h1', html);
       expect(
         header.length,
-        `Could not find a h1 tag after clicking the about button (span). Use the on() method for dynamic event listener`
+        `Could not find an 'h1' tag on the page after clicking the 'about' button (span). Use the jQuery on() method to create a dynamic event listener`
       ).toBe(1);
       headerHTML = await page.$eval('h1', (element) => {
         return element.innerHTML;
       });
       expect(
         headerHTML,
-        `Could not find a h1 tag with 'The Team:' as innerHTML after clicking the about button (span), instead we got - ${headerHTML}. Make sure you use the 'aboutHeader' variable and use the on() method for dynamic event listener`
+        `After clicking the 'about' button (span), we could not find an 'h1' tag with 'The Team:' as it's innerHTML. Instead we got - ${headerHTML}. Make sure you use the 'aboutHeader' variable and use the jQuery on() method to create a dynamic event listener`
       ).toBe('The Team:');
 
       await page.click('#about');
@@ -59,7 +59,7 @@ describe('Exercise 3', () => {
       header = $('h1', html);
       expect(
         header.length,
-        `Could not find one h1 tag after clicking the about button (span) twice, instead found ${header.length}. Make sure to empty the container element before appending it with the h1 tag`
+        `Could not find 'one' 'h1' tag after clicking the about button (span) twice, instead found ${header.length}. Make sure to empty the container element before appending the 'h1' tag to it`
       ).toBe(1);
       done();
     } catch (error) {
@@ -67,21 +67,21 @@ describe('Exercise 3', () => {
     }
   });
 
-  it(`Should display five team members details after clicking on the about button (span)`, async function (done) {
+  it(`Clicking on the 'about' button (span) should display five team member details on the page`, async function (done) {
     try {
       await page.click('#about');
       let html = await page.content();
       let teamMembers = $('.team-member', html);
       expect(
         teamMembers.length,
-        `Could not find five div's with class of team-member after clicking the about button (span). Instead found ${teamMembers.length} div's with class or team-member. Use the on() method for dynamic event listener`
+        `Could not find five 'div's with a class of 'team-member' after clicking the 'about' button (span). Instead found ${teamMembers.length} divs with class of team-member. Use the jQuery on() method to create a dynamic event listener`
       ).toBe(5);
       await page.click('#about');
       html = await page.content();
       teamMembers = $('.team-member', html);
       expect(
         teamMembers.length,
-        `Could not find five div's with class of team-member after clicking the about button (span) twice. Instead found ${teamMembers.length} div's with class or team-member. Make sure to empty the container element before appending it with the team-members div's`
+        `Could not find five 'div's with a class of 'team-member' after clicking the about button (span) twice. Instead found ${teamMembers.length} divs with class of 'team-member'. Make sure to empty the container element before appending the 'team-member' divs to it`
       ).toBe(5);
       done();
     } catch (error) {
@@ -89,7 +89,7 @@ describe('Exercise 3', () => {
     }
   });
 
-  it(`Should change the color of the name of the team member after clicking it`, async function (done) {
+  it(`Clicking on the name of a team member should change its' color`, async function (done) {
     try {
       await page.click('#about');
       let html = await page.content();
@@ -98,18 +98,18 @@ describe('Exercise 3', () => {
 
       expect(
         namesArr[0].attribs.style,
-        `Before clicking on div with class=name of ${innerText}, it has an attribue of style which it should not have`
+        `Before clicking on div with class of 'name' and innerHTML of ${innerText}, it has style attribue which it should not have`
       ).toBeFalsy();
       await page.click('.name');
       html = await page.content();
       namesArr = $('.name', html);
       expect(
         namesArr[0].attribs.style.includes('rgb(255, 110, 159)'),
-        `After clicking on div with class=name of ${innerText}, the color did not change to the right color, and instead we got ${namesArr[0].attribs.style}. Note that after clicking, the color should change to - #ff6e9f (rgb(255, 110, 159)). Use 'this' keyword to select the correct div and the 'css' keywrod to change its color`
+        `After clicking on div with class of 'name' and innerHTML of ${innerText}, the color did not change to the correct color, and instead we got ${namesArr[0].attribs.style}. Note that after clicking, the color should change to - #ff6e9f (rgb(255, 110, 159)). Use the 'this' keyword to select the correct div and the jQuery'css()' method to change its' color`
       ).toBeTruthy();
       expect(
         namesArr[0].attribs.style.includes('color'),
-        `After clicking on div with class=name of ${innerText}, the color did not change to the right color, and instead we got ${namesArr[0].attribs.style}. Note that after clicking, the color should change to - #ff6e9f (rgb(255, 110, 159)). Use 'this' keyword to select the correct div and the 'css' keywrod to change its color`
+        `After clicking on div with class of 'name' and innerHTML of ${innerText}, the color did not change to the correct color, and instead we got ${namesArr[0].attribs.style}. Note that after clicking, the color should change to - #ff6e9f (rgb(255, 110, 159)). Use the 'this' keyword to select the correct div and the jQuery'css()' method to change its' color`
       ).toBeTruthy();
       done();
     } catch (error) {
@@ -117,7 +117,7 @@ describe('Exercise 3', () => {
     }
   });
 
-  it(`Should change the color of the position of the team member after clicking on it's name`, async function (done) {
+  it(`Clicking on the name of a team member should change the color of the 'position' of that team member`, async function (done) {
     try {
       await page.click('#about');
       let html = await page.content();
@@ -125,7 +125,7 @@ describe('Exercise 3', () => {
       let innerText = await page.evaluate(() => document.querySelectorAll('.name')[0].innerText);
       expect(
         posArr[0].attribs.style,
-        `Before clicking on div with class=name of ${innerText}, it's position div has an attribue of style which it should not have`
+        `Before clicking on a div with class of 'name' and innerHTML of ${innerText}, it's 'position' div had a style attribue which it should not have`
       ).toBeFalsy();
 
       await page.click('.name');
@@ -133,11 +133,11 @@ describe('Exercise 3', () => {
       posArr = $('.position', html);
       expect(
         posArr[0].attribs.style.includes('rgb(255, 110, 159)'),
-        `After clicking on div with class=name of ${innerText}, the color of its position div did not change to the right color, and instead we got ${posArr[0].attribs.style}. Note that after clicking, the color should change to - #ff6e9f (rgb(255, 110, 159)). Use 'this', 'closest' and 'find' keywords to select the correct div, and the 'css' keywrod to change its color`
+        `After clicking on a div with class of 'name' and innerHTML of ${innerText}, the color of its 'position' div did not change to the correct color, and instead we got ${posArr[0].attribs.style}. Note that after clicking on the name, the color should change to - #ff6e9f (rgb(255, 110, 159)). Use the 'this', 'closest' and 'find' keywords to select the correct div, and the jQuery 'css()' method to change its color`
       ).toBeTruthy();
       expect(
         posArr[0].attribs.style.includes('color'),
-        `After clicking on div with class=name of ${innerText}, the color of its position div did not change to the right color, and instead we got ${posArr[0].attribs.style}. Note that after clicking, the color should change to - #ff6e9f (rgb(255, 110, 159)). Use 'this', 'closest' and 'find' keywords to select the correct div, and the 'css' keywrod to change its color`
+        `After clicking on a div with class of name and innerHTML of ${innerText}, the color of its 'position' div did not change to the correct color, and instead we got ${posArr[0].attribs.style}. Note that after clicking on the name, the color should change to - #ff6e9f (rgb(255, 110, 159)). Use the 'this', 'closest' and 'find' keywords to select the correct div, and the jQuery 'css()' method to change its color`
       ).toBeTruthy();
       done();
     } catch (error) {
@@ -145,7 +145,7 @@ describe('Exercise 3', () => {
     }
   });
 
-  it(`Should change the background color of the div (class=team-member) which contain the name and position of a team member after clicking on the name`, async function (done) {
+  it(`Clicking on the 'name' should change the background color of the div with class of 'team-member' which contains the clicked name`, async function (done) {
     try {
       await page.click('#about');
       let html = await page.content();
@@ -153,7 +153,7 @@ describe('Exercise 3', () => {
       let innerText = await page.evaluate(() => document.querySelectorAll('.name')[0].innerText);
       expect(
         membersArr[0].attribs.style,
-        `Before clicking on div with class=name of ${innerText}, it's ancestor div (class=team-member) has an attribue of style which it should not have`
+        `Before clicking on div with class of 'name' and innerHTML of ${innerText}, it's ancestor div (with class 'team-member') has a style attribue which it should not have`
       ).toBeFalsy();
 
       await page.click('.name');
@@ -161,11 +161,11 @@ describe('Exercise 3', () => {
       membersArr = $('.team-member', html);
       expect(
         membersArr[0].attribs.style.includes('rgb(30, 47, 77)'),
-        `After clicking on div with class=name of ${innerText}, the background color of its ancestor div (class=team-member) did not change to the right color, and instead we got ${membersArr[0].attribs.style}. Note that after clicking, the color should change to - #1e2f4d (rgb(30, 47, 77))). Use 'this' and 'closest' keywords to select the correct div, and the 'css' keywrod to change its background color`
+        `After clicking on a div with class of name and innerHTML of ${innerText}, the background color of its ancestor div (with class 'team-member') did not change to the correct color, and instead we got ${membersArr[0].attribs.style}. Note that after clicking on the name, the background color should change to - #1e2f4d (rgb(30, 47, 77))). Use the 'this' and 'closest' keywords to select the correct div, and the jQuery 'css()' method to change its background color`
       ).toBeTruthy();
       expect(
         membersArr[0].attribs.style.includes('background-color'),
-        `After clicking on div with class=name of ${innerText}, the background color of its ancestor div (class=team-member) did not change to the right color, and instead we got ${membersArr[0].attribs.style}. Note that after clicking, the color should change to - #1e2f4d (rgb(30, 47, 77))). Use 'this' and 'closest' keywords to select the correct div, and the 'css' keywrod to change its background color`
+        `After clicking on a div with class of 'name' and innerHTML of ${innerText}, the background color of its ancestor div (with class 'team-member') did not change to the correct color, and instead we got ${membersArr[0].attribs.style}. Note that after clicking on the name, the background color should change to - #1e2f4d (rgb(30, 47, 77))). Use the 'this' and 'closest' keywords to select the correct div, and the jQuery 'css()' method to change its background color`
       ).toBeTruthy();
       done();
     } catch (error) {
--- exercises/ex-4.test.js
+++ exercises/ex-4.test.js
@@ -8,40 +8,40 @@ describe('Exercise 4', () => {
     await page.goto('file://' + path.join(__dirname, '..', '..', 'src', 'index.html'));
   });
 
-  it(`index.html should not be modified`, async function (done) {
+  it(`The index.html file should not be modified`, async function (done) {
     const htmlContent = fs.readFileSync(
       path.join(__dirname, '..', '..', 'src', 'index.html'),
       'utf8'
     );
     expect(
       htmlContent.includes('id="search"'),
-      `index.html contain an element with id of 'search', although it should not be modified. Reminder: you should not add any elements to the index.html file ; make sure you write your code only in the main.js file`
+      `The index.html file contains an element with id of 'search', although it should not be modified. Reminder: you should not add any elements to the index.html file - make sure you write your code only in the main.js file`
     ).toBeFalsy();
     expect(
       htmlContent.includes('id="search-field"'),
-      `index.html contain an element with id of search-field, although it should not be modified. Reminder: you should not add any elements to the index.html file ; make sure you write your code only in the main.js file`
+      `The index.html file contains an element with id of 'search-field', although it should not be modified. Reminder: you should not add any elements to the index.html file - make sure you write your code only in the main.js file`
     ).toBeFalsy();
     expect(
       htmlContent.includes('id="search-btn"'),
-      `index.html contain an element with id of search-btn, although it should not be modified. Reminder: you should not add any elements to the index.html file ; make sure you write your code only in the main.js file`
+      `The index.html file contains an element with id of 'search-btn', although it should not be modified. Reminder: you should not add any elements to the index.html file - make sure you write your code only in the main.js file`
     ).toBeFalsy();
     expect(
       htmlContent.includes('id="search-results"'),
-      `index.html contain an element with id of search-results, although it should not be modified. Reminder: you should not add any elements to the index.html file ; make sure you write your code only in the main.js file`
+      `The index.html file contains an element with id of 'search-results', although it should not be modified. Reminder: you should not add any elements to the index.html file - make sure you write your code only in the main.js file`
     ).toBeFalsy();
     done();
   });
 
-  it(`Should use jQuery and not DOM commands`, async function (done) {
+  it(`You should use jQuery and not vanilla JS DOM methods`, async function (done) {
     const mainContent = fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'main.js'), 'utf8');
     expect(
       mainContent.includes('$'),
-      `Could not find '$' (jQuery) in the main.js file. Rememeber that you should use jQuery and not DOM commands`
+      `Could not find '$' (jQuery) in the main.js file. Rememeber that you should use jQuery and not vanilla JS DOM operations`
     ).toBeTruthy();
     done();
   });
 
-  it(`Should display an input with id=search-field, button with id=search-btn, and div with id="search-results", after clicking on the search button (span)`, async function (done) {
+  it(`Clicking on the search button (span) should display an input with id of 'search-field', button with id of 'search-btn', and div with id of 'search-results'`, async function (done) {
     try {
       await page.click('#search');
       let html = await page.content();
@@ -50,23 +50,23 @@ describe('Exercise 4', () => {
       let searchResults = $('#search-results', html);
       expect(
         searchField.length,
-        `Could not find a tag with id=search-field after clicking the search button (span). Use the on() method for dynamic event listener`
+        `Could not find a tag with id of 'search-field' after clicking the 'search' button (span). Use the jQuery on() method to create a dynamic event listener`
       ).toBe(1);
       expect(
         searchField[0].name,
-        `The tag with id=search-field is not an 'input' tag, instead we got - ${searchField[0].name}. Make sure you're using the 'input' tag`
+        `The tag with id of 'search-field' is not an 'input' tag, instead we got - ${searchField[0].name}. Make sure you're using the 'input' tag`
       ).toBe('input');
       expect(
         searchBtn.length,
-        `Could not find a tag with id=search-btn after clicking the search button (span). Use the on() method for dynamic event listener`
+        `Could not find a tag with id of 'search-btn' after clicking the 'search' button (span). Use the jQuery on() method to create a dynamic event listener`
       ).toBe(1);
       expect(
         searchBtn[0].name,
-        `The tag with id=search-btn is not an 'button' tag, instead we got - ${searchBtn[0].name}. Make sure you're using the 'button' tag`
+        `The tag with id of 'search-btn' is not a 'button' tag, instead we got - ${searchBtn[0].name}. Make sure you're using a 'button' tag`
       ).toBe('button');
       expect(
         searchResults.length,
-        `Could not find a tag with id=search-results after clicking the search button (span). Use the on() method for dynamic event listener`
+        `Could not find a tag with id of 'search-results' after clicking the 'search' button (span). Use the jQuery on() method to create a dynamic event listener`
       ).toBe(1);
 
       await page.click('#search');
@@ -74,7 +74,7 @@ describe('Exercise 4', () => {
       searchField = $('#search-field', html);
       expect(
         searchField.length,
-        `Could not find one tag with id=search-field after clicking the search button (span) twice. Use the on() method for dynamic event listener and make sure you empty the container`
+        `Could not find 'one' tag with id of 'search-field' after clicking the 'search' button (span) twice. Use the jQuery on() method to create a dynamic event listener and make sure you empty the container before you append to it`
       ).toBe(1);
       done();
     } catch (error) {
@@ -82,7 +82,7 @@ describe('Exercise 4', () => {
     }
   });
 
-  it(`Should display a message containing an input after clicking the Search button`, async function (done) {
+  it(`Clicking the 'Search' button should display a message containing the value of the input`, async function (done) {
     try {
       await page.click('#search');
       await page.$eval('#search-field', (el) => (el.value = 'Data'));
@@ -92,7 +92,7 @@ describe('Exercise 4', () => {
       );
       expect(
         innerText.includes('Data'),
-        `After insert the string 'Data' to the input (id=#search-field) and clicking the button (id=search), there was not a message in a div (id=search-results) containing the searched word. This is what we got in the div (id=search-results): ${innerText}`
+        `After inserting the string 'Data' to the input (with id of 'search-field') and clicking the 'Search' button (with id of 'search'), there was not a message in the div with id of 'search-results' containing the searched word. This is what we found in the div (with id of 'search-results'): ${innerText}`
       ).toBeTruthy();
       done();
     } catch (error) {

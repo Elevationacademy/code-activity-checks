5be5331d60535ff43e3620a3975c62ed942db040
danbrud <dannybrudner@users.noreply.github.com>
2020-05-25 11:02:31 +0300

    modified english ex2

--- exercises/ex-2.test.js
+++ exercises/ex-2.test.js
@@ -1,70 +1,70 @@
 describe('Exercise 2', function () {
 
-  it(`moviesModule should return 'addMovie', 'getMovies', 'deleteMovie', and 'modifyMovies' functions`, function () {
+  it(`The 'moviesModule' should return the 'addMovie', 'getMovies', 'deleteMovie', and 'modifyMovies' functions as properties of an onject`, function () {
     try {
       const { moviesModule} = require('../../src/exercise2');
       const m = moviesModule()
-      expect(m.addMovie, "moviesModule do not return a 'addMovie' function. Make sure you return an object with 'addMovie' key-value").toBeTruthy()
-      expect(m.getMovies, "moviesModule do not return a 'getMovies' function. Make sure you return an object with 'getMovies' key-value").toBeTruthy()
-      expect(m.deleteMovie, "moviesModule do not return a 'deleteMovie' function. Make sure you return an object with 'deleteMovie' key-value").toBeTruthy()
-      expect(m.modifyMovies, "moviesModule do not return a 'modifyMovies' function. Make sure you return an object with 'modifyMovies' key-value").toBeTruthy()
-      expect(typeof(m.addMovie), `moviesModule do not return a 'addMovie' function. The type of 'addMovie' is ${typeof(m.addMovie)} instead of a 'function'`).toBe('function')
-      expect(typeof(m.getMovies), `moviesModule do not return a 'getMovies' function. The type of 'getMovies' is ${typeof(m.getMovies)} instead of a 'function'`).toBe('function')
-      expect(typeof(m.deleteMovie), `moviesModule do not return a 'deleteMovie' function. The type of 'deleteMovie' is ${typeof(m.deleteMovie)} instead of a 'function'`).toBe('function')
-      expect(typeof(m.modifyMovies), `moviesModule do not return a 'modifyMovies' function. The type of 'modifyMovies' is ${typeof(m.modifyMovies)} instead of a 'function'`).toBe('function')
+      expect(m.addMovie, "The 'moviesModule' does not return an 'addMovie' function. Make sure you return an object with 'addMovie' as a key-value pair").toBeTruthy()
+      expect(m.getMovies, "The 'moviesModule' does not return a 'getMovies' function. Make sure you return an object with 'getMovies' as a key-value pair").toBeTruthy()
+      expect(m.deleteMovie, "The 'moviesModule' does not return a 'deleteMovie' function. Make sure you return an object with 'deleteMovie' as a key-value pair").toBeTruthy()
+      expect(m.modifyMovies, "The 'moviesModule' does not return a 'modifyMovies' function. Make sure you return an object with 'modifyMovies' as a key-value pair").toBeTruthy()
+      expect(typeof(m.addMovie), `The 'moviesModule' does not return an 'addMovie' function. The type of 'addMovie' is ${typeof(m.addMovie)} instead of 'function'`).toBe('function')
+      expect(typeof(m.getMovies), `The 'moviesModule' does not return a 'getMovies' function. The type of 'getMovies' is ${typeof(m.getMovies)} instead of 'function'`).toBe('function')
+      expect(typeof(m.deleteMovie), `The 'moviesModule' does not return a 'deleteMovie' function. The type of 'deleteMovie' is ${typeof(m.deleteMovie)} instead of 'function'`).toBe('function')
+      expect(typeof(m.modifyMovies), `The 'moviesModule' does not return a 'modifyMovies' function. The type of 'modifyMovies' is ${typeof(m.modifyMovies)} instead of 'function'`).toBe('function')
 
     } catch (error) {
       expect(false, `${error}`).toBeTruthy();
     }
   });
 
-  it(`moviesModule should return an 'addMovie' function which push a movie string to the 'movies' array and a 'getMovies' function which return the 'movies' array`, function () {
+  it(`The 'moviesModule' should return an 'addMovie' function which pushes a movie string to the 'movies' array and a 'getMovies' function which returns the 'movies' array`, function () {
     try {
       const { moviesModule} = require('../../src/exercise2');
       const m = moviesModule()
       m.addMovie('Star Trek')
       const movies = m.getMovies()
-      expect(movies, `Expected to get ["The Godfather", "Back to the Future", "Star Trek"] after invoking the 'addMovie' function with the movie "Star Trek" and then invoking the 'getMovies' function, but instead got - ${JSON.stringify(movies)}.  Make sure you create an 'movies' array inside the 'moviesModule' with ['The Godfather', 'Back to the Future'], and that 'addMovie' function push a parameter that was received to that array`).toEqual(['The Godfather', 'Back to the Future', 'Star Trek'])
+      expect(movies, `After invoking the 'addMovie' function with the movie "Star Trek" and then invoking the 'getMovies' function, expected to receive ["The Godfather", "Back to the Future", "Star Trek"], but instead got - ${JSON.stringify(movies)}.  Make sure you create an 'movies' array inside the 'moviesModule' with ['The Godfather', 'Back to the Future'], and that the 'addMovie' function pushes the argument that was received to that array`).toEqual(['The Godfather', 'Back to the Future', 'Star Trek'])
     } catch (error) {
       expect(false, `${error}`).toBeTruthy();
     }
   });
 
-  it(`moviesModule should return an 'deleteMovie' function which find a given movie in the 'movies' array and delete it`, function () {
+  it(`The 'moviesModule' should return a 'deleteMovie' function that finds a given movie in the 'movies' array and deletes it`, function () {
     try {
       const { moviesModule} = require('../../src/exercise2');
       const m = moviesModule()
       m.deleteMovie('The Godfather')
       const movies = m.getMovies()
-      expect(movies, `Expected to get ["Back to the Future"] after invoking the 'deleteMovie' function with the movie 'The Godfather' and then invoking the 'getMovies' function, but instead got - ${JSON.stringify(movies)}.  Make sure you create an 'movies' array inside the 'moviesModule' with ['The Godfather', 'Back to the Future'], and that 'deleteMovie' function can receive a movie as a parameter, find it in the 'movies' array and delete it (use 'splice' function)`).toEqual(['Back to the Future'])
+      expect(movies, `After invoking the 'deleteMovie' function with the movie 'The Godfather' and then invoking the 'getMovies' function, expected to get ["Back to the Future"], but instead got - ${JSON.stringify(movies)}.  Make sure you create a 'movies' array inside the 'moviesModule' with ['The Godfather', 'Back to the Future'], and that the 'deleteMovie' function can receive a movie as a parameter, find it in the 'movies' array, and delete it (use the 'splice' method)`).toEqual(['Back to the Future'])
     } catch (error) {
       expect(false, `${error}`).toBeTruthy();
     }
   });
 
-  it(`makeSequal function should receive an array of strings and add ': Part II' to each one of them`, function () {
+  it(`The 'makeSequel' function should receive an array of strings and add ': Part II' to each one of them`, function () {
     try {
       const { makeSequal} = require('../../src/exercise2');
       const movies = ['The Godfather', 'Back to the Future']
       makeSequal(movies)
-      expect(movies, `Expected to get ["The Godfather: Part II", "Back to the Future: Part II"] after invoking the 'makeSequal' function with an array of ["The Godfather", "Back to the Future"], but instead got - ${JSON.stringify(movies)}.  Make sure 'makeSequal' iterate the array and add ': Part II' to each one of its elements`).toEqual(['The Godfather: Part II', 'Back to the Future: Part II'])
+      expect(movies, `After invoking the 'makeSequel' function with an array of ["The Godfather", "Back to the Future"], expected to get ["The Godfather: Part II", "Back to the Future: Part II"], but instead got - ${JSON.stringify(movies)}.  Make sure 'makeSequel' iterates over the array and adds ': Part II' to each one of its elements`).toEqual(['The Godfather: Part II', 'Back to the Future: Part II'])
     } catch (error) {
       expect(false, `${error}`).toBeTruthy();
     }
   });
 
-  it(`addCounter function should receive an array of strings and add a counter in the beginning of each element of the array`, function () {
+  it(`The 'addCounter' function should receive an array of strings and add a counter to the beginning of each element in the array`, function () {
     try {
       const { addCounter} = require('../../src/exercise2');
       const movies = ['The Godfather', 'Back to the Future']
       addCounter(movies)
-      expect(movies, `Expected to get ["1. The Godfather", "2. Back to the Future"] after invoking the 'addCounter' function with an array of ["The Godfather", "Back to the Future"], but instead got - ${JSON.stringify(movies)}.  Make sure 'addCounter' iterate the array and add a counter, a dot and a space in the beginning of each string in the array. Also make sure that the counter start with 1 and not 0`).toEqual(['1. The Godfather', '2. Back to the Future'])
+      expect(movies, `After invoking the 'addCounter' function with an array of ["The Godfather", "Back to the Future"], expected to get ["1. The Godfather", "2. Back to the Future"], but instead got - ${JSON.stringify(movies)}.  Make sure 'addCounter' iterates over the array and adds a counter, a dot, and a space, to the beginning of each string in the array. Also make sure that the counter starts with 1 and not 0`).toEqual(['1. The Godfather', '2. Back to the Future'])
     } catch (error) {
       expect(false, `${error}`).toBeTruthy();
     }
   });
 
-  it(`moviesModule should return an 'modifyMovies' function which receive a function and invoke it on the 'movies' array`, function () {
+  it(`The 'moviesModule' should return a 'modifyMovies' function which receives a function and invokes it with the 'movies' array`, function () {
     try {
       const { moviesModule } = require('../../src/exercise2');
       const makeSequal = function (movies) {
@@ -75,25 +75,25 @@ describe('Exercise 2', function () {
       const m = moviesModule()
       m.modifyMovies(makeSequal)
       const movies = m.getMovies()
-      expect(movies, `Expected to get ["The Godfather: Part II", "Back to the Future: Part II"] after invoking the 'modifyMovies' function with a 'makeSequal' function (which was created by us), but instead got - ${JSON.stringify(movies)}.  Make sure 'modifyMovies' receive a function as a parameter and invokes it with the 'movies' array`).toEqual(["The Godfather: Part II","Back to the Future: Part II"])
+      expect(movies, `After invoking the 'modifyMovies' function with a 'makeSequel' function (which was created by us), expected to get ["The Godfather: Part II", "Back to the Future: Part II"], but instead got - ${JSON.stringify(movies)}.  Make sure 'modifyMovies' receives a function as a parameter and invokes it with the 'movies' array`).toEqual(["The Godfather: Part II","Back to the Future: Part II"])
 
     } catch (error) {
       expect(false, `${error}`).toBeTruthy();
     }
   });
 
-  it(`moviesModule should return an 'modifyMovies' function which receive a function and invoke it on the 'movies' array. This function should be able to use 'makeSequal' and 'addCounter' functions properly`, function () {
+  it(`The 'moviesModule' should return a 'modifyMovies' function which receives a function and invokes it with the 'movies' array. This function should be able to use the 'makeSequel' and 'addCounter' functions properly`, function () {
     try {
       const { moviesModule, makeSequal, addCounter } = require('../../src/exercise2');
       const m = moviesModule()
       m.modifyMovies(makeSequal)
       const movies = m.getMovies()
-      expect(movies, `Expected to get ["The Godfather: Part II", "Back to the Future: Part II"] after invoking the 'modifyMovies' function with the 'makeSequal' function, but instead got - ${JSON.stringify(movies)}.  Make sure 'modifyMovies' receive a function as a parameter and invokes it with the 'movies' array`).toEqual(["The Godfather: Part II","Back to the Future: Part II"])
+      expect(movies, `After invoking the 'modifyMovies' function with the 'makeSequel' function, expected to get ["The Godfather: Part II", "Back to the Future: Part II"], but instead got - ${JSON.stringify(movies)}. Make sure 'modifyMovies' receives a function as a parameter and invokes it with the 'movies' array`).toEqual(["The Godfather: Part II","Back to the Future: Part II"])
 
       const m2 = moviesModule()
       m2.modifyMovies(addCounter)
       const movies2 = m2.getMovies()
-      expect(movies2, `Expected to get ["1. The Godfather", "2. Back to the Future"] after invoking the 'modifyMovies' function with the 'addCounter' function, but instead got - ${JSON.stringify(movies2)}.  Make sure 'modifyMovies' receive a function as a parameter and invokes it with the 'movies' array`).toEqual(["1. The Godfather","2. Back to the Future"])
+      expect(movies2, `After invoking the 'modifyMovies' function with the 'addCounter' function, expected to get ["1. The Godfather", "2. Back to the Future"], but instead got - ${JSON.stringify(movies2)}. Make sure 'modifyMovies' receives a function as a parameter and invokes it with the 'movies' array`).toEqual(["1. The Godfather","2. Back to the Future"])
 
     } catch (error) {
       expect(false, `${error}`).toBeTruthy();

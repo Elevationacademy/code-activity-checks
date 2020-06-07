865a835b085ce0e102e9eeadec0a79778283a5e6
danbrud <dannybrudner@users.noreply.github.com>
2020-06-07 19:29:32 +0300

    english ex2

--- exercises/ex-2.test.js
+++ exercises/ex-2.test.js
@@ -20,7 +20,7 @@ describe('Exercise 2', () => {
     html = await page.content();
   });
 
-  it(`There should be an element with an id of 'logo' inside an element with an id of 'container'. Inside this element there should be an element with id of 'logo-title'`, async function (done) {
+  it(`There should be an element with an id of 'logo' inside an element with an id of 'container'. Inside this element there should be an element with an id of 'logo-title'`, async function (done) {
     try {
       const container = $('#container', html);
       const logo = $('#logo', container);
@@ -39,13 +39,13 @@ describe('Exercise 2', () => {
     }
   });
 
-  it(`The element with id of 'logo' should have a background-image property of the given url's jpg picture`, async function (done) {
+  it(`The element with an id of 'logo' should have a background-image property with the given url's jpg picture`, async function (done) {
     try {
       const logoBGImage = await getStyleFromElement(page, '#logo', 'backgroundImage');
-      expect(logoBGImage, `Could not find an element with id of 'logo'`).toBeTruthy();
+      expect(logoBGImage, `Could not find an element with an id of 'logo'`).toBeTruthy();
       expect(
         logoBGImage.includes('https://cdn.statically.io/img/wallpapercave.com/wp/wp2858550.jpg'),
-        `The element with id of 'logo' do not have a background-image property of the given url's jpg picture. Instead it has the following as a background-image propety: ${logoBGImage}`
+        `The element with an id of 'logo' does not have a background-image property with the given url's jpg picture. Instead it has the following background-image propety: ${logoBGImage}`
       ).toBeTruthy();
       done();
     } catch (error) {
@@ -53,13 +53,13 @@ describe('Exercise 2', () => {
     }
   });
 
-  it(`The element with id of 'logo-title' should have an inner HTML of 'The Pink Elevations'`, async function (done) {
+  it(`The element with an id of 'logo-title' should have an innerHTML of 'The Pink Elevations'`, async function (done) {
     try {
       const logoTitleHTML = $('#logo-title', html).html();
-      expect(logoTitleHTML, `Could not find an element with id of 'logo-title'`).toBeTruthy();
+      expect(logoTitleHTML, `Could not find an element with an id of 'logo-title'`).toBeTruthy();
       expect(
         logoTitleHTML.toLowerCase().includes('the pink elevations'),
-        `The element with id of 'logo-title' do not have an inner HTML of 'The Pink Elevations', instead it has inner HTML of - ${logoTitleHTML}`
+        `The element with an id of 'logo-title' does not have innerHTML with 'The Pink Elevations', instead it has innerHTML of - ${logoTitleHTML}`
       ).toBeTruthy();
       done();
     } catch (error) {
@@ -67,7 +67,7 @@ describe('Exercise 2', () => {
     }
   });
 
-  it(`The element with id of 'logo-title' should have its inner HTML centered with color of #1e2f4d (rgb(30, 47, 77)) and font-family of "Miriam Libre", sans-serif`, async function (done) {
+  it(`The element with an id of 'logo-title' should have its innerHTML centered with the color #1e2f4d (rgb(30, 47, 77)) and font-family of "Miriam Libre", sans-serif`, async function (done) {
     try {
       const elems = ['body', '#container', '#logo', '#logo-title'];
       let isCentered = false,
@@ -77,22 +77,22 @@ describe('Exercise 2', () => {
         const elementTextAlign = await getStyleFromElement(page, e, 'textAlign');
         const elementColor = await getStyleFromElement(page, e, 'color');
         const elementFont = await getStyleFromElement(page, e, 'fontFamily');
-        expect(elementFont, `Could not find an element with id of '${e}'`).toBeTruthy();
+        expect(elementFont, `Could not find an element with an id of '${e}'`).toBeTruthy();
         if (elementTextAlign.includes('center')) isCentered = true;
         if (elementColor.includes('rgb(30, 47, 77)')) isColor = true;
         if (elementFont.includes('Miriam Libre')) isFont = true;
       }
       expect(
         isCentered,
-        `The element with id of 'logo-title' (and its ancestors) do not have a text-align property of 'center'`
+        `The element with an id of 'logo-title' (and its ancestors) do not have a text-align property of 'center'`
       ).toBeTruthy();
       expect(
         isColor,
-        `The element with id of 'logo-title' (and its ancestors) do not have a color property of #1e2f4d (rgb(30, 47, 77))`
+        `The element with an id of 'logo-title' (and its ancestors) do not have a color property of #1e2f4d (rgb(30, 47, 77))`
       ).toBeTruthy();
       expect(
         isFont,
-        `The element with id of 'logo-title' (and its ancestors) do not have a font-family of "Miriam Libre", sans-serif`
+        `The element with an id of 'logo-title' (and its ancestors) do not have a font-family of "Miriam Libre", sans-serif`
       ).toBeTruthy();
       done();
     } catch (error) {
@@ -100,12 +100,12 @@ describe('Exercise 2', () => {
     }
   });
 
-  it(`The element with id of 'logo-title' should be placed around the middle of the element with id of "logo"`, async function (done) {
+  it(`The element with an id of 'logo-title' should be placed around the middle of the element with an id of "logo"`, async function (done) {
     try {
       const logoElem = await page.$('#logo');
-      expect(logoElem, `Could not find an element with id of 'logo'`).toBeTruthy();
+      expect(logoElem, `Could not find an element with an id of 'logo'`).toBeTruthy();
       const logoTitleElem = await page.$('#logo-title');
-      expect(logoTitleElem, `Could not find an element with id of 'logo-title'`).toBeTruthy();
+      expect(logoTitleElem, `Could not find an element with an id of 'logo-title'`).toBeTruthy();
       const logoBox = await logoElem.boxModel();
       const logoTitleBox = await logoTitleElem.boxModel();
       const dist1 = logoTitleBox.margin[0].y - logoBox.margin[0].y;
@@ -113,7 +113,7 @@ describe('Exercise 2', () => {
       const isMiddle = dist1 / dist2 > 0.75 || dist1 / dist2 < 1.4;
       expect(
         isMiddle,
-        `The element with id of 'logo-title' is not located around the middle of the element with id of "logo". Use position: 'absolute' and 'top' to locate the "logo-title" element around the middle of the 'logo' element`
+        `The element with an id of 'logo-title' is not located around the middle of the element with an id of "logo". Hint: Use position 'absolute' and 'top' to position the "logo-title" element around the middle of the 'logo' element`
       ).toBeTruthy();
       done();
     } catch (error) {
@@ -121,19 +121,19 @@ describe('Exercise 2', () => {
     }
   });
 
-  it(`The height of the logo image (the element with the id of 'logo') should be bigger than the height of the element with id of 'logo-title'`, async function (done) {
+  it(`The height of the logo image (the element with the id of 'logo') should be larger than the height of the element with an id of 'logo-title'`, async function (done) {
     try {
       const logoElem = await page.$('#logo');
-      expect(logoElem, `Could not find an element with id of 'logo'`).toBeTruthy();
+      expect(logoElem, `Could not find an element with an id of 'logo'`).toBeTruthy();
       const logoTitleElem = await page.$('#logo-title');
-      expect(logoTitleElem, `Could not find an element with id of 'logo-title'`).toBeTruthy();
+      expect(logoTitleElem, `Could not find an element with an id of 'logo-title'`).toBeTruthy();
       const logoBox = await logoElem.boxModel();
       const logoTitleBox = await logoTitleElem.boxModel();
       const logoHeight = logoBox.height
       const logoTitleHeight = logoTitleBox.height
       expect(
         logoHeight>logoTitleHeight,
-        `The height of the logo image (the element with the id of 'logo') is not bigger than the height of the element with id of 'logo-title'`
+        `The height of the logo image (the element with the id of 'logo') is not larger than the height of the element with an id of 'logo-title'`
       ).toBeTruthy();
       done();
     } catch (error) {

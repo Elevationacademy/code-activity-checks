e5d6c092ed1ab6fc311751ba89511676e0543193
danbrud <dannybrudner@users.noreply.github.com>
2020-06-08 18:05:16 +0300

    removed an expect

--- exercises/ex-4.test.js
+++ exercises/ex-4.test.js
@@ -13,11 +13,9 @@ describe('Exercise 4', () => {
     try {
       await git.checkout([branchName])
       const log = await git.log()
-      const srcCommit = getCommitByMessage(log, commitMessage)
       const commit = log.latest
 
       expect(commit.message, `When reseting to the previous commit, that commit should now be at the head of the logs. The message of the HEAD commit should contain ${commitMessage}. Instead the message was ${commit.message}`).toContain(commitMessage)
-      expect(commit.hash, `When reseting to the previous commit, that commit should now be at the head of the logs. The commit at the HEAD should have a hash of '${srcCommit.hash}'. Instead the hash was '${commit.hash}'`).toContain(srcCommit.hash)
       expect(log.all.length, `Could not find enough commits on the '${branchName}' branch. Expected at least 4 commits instead found ${log.all.length}`).toBeGreaterThan(3)
     } catch (e) {
       expect(true, `${e.message}`).toBeFalsy()
